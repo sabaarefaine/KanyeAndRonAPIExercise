02e7b1fc9e8a292fa0bd2c870dcc676f36b7c99f
Saba Arefaine <arefainesaba@gmail.com>
2021-05-04 16:53:57 -0700

    ron and kanye chat

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You walk into a bar and overhear Kanye West and Ron Swanson having a chat...");
+
+            for (int i = 0; i < 5; i++)
+            {
+                QuoteGenerator.KanyeQuote();
+
+                QuoteGenerator.RonQuote();
+            }
+        }
+    }
+}
